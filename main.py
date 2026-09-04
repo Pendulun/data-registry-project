fd93b8cefe7cd69d0efb539c443a4c73e99691a5
Daniel Campos <danielcampos14@hotmail.com.br>
2026-09-04 18:45:16 -0300

    feat: First endpoints

---
+++ main.py
@@ -0,0 +1,80 @@
+import datetime
+import dotenv
+from fastapi import FastAPI, Form, UploadFile
+from pydantic import BaseModel
+from typing import Annotated
+import src.db.conn as conn
+
+dotenv.load_dotenv()
+
+app = FastAPI()
+
+datasets = dict()
+
+
+class Dataset(BaseModel):
+    name: str
+    description: str
+
+
+class Version(BaseModel):
+    id: int
+    owner: str
+
+
+class FileUpload(BaseModel):
+    dataset: Dataset
+    version: Version
+    file: UploadFile
+
+
+@app.post("/create/")
+def create_dataset(dataset: Dataset):
+    conn.add_dataset(dataset.name, dataset.description)
+    return {"status": "200"}
+
+
+@app.get("/list")
+def list_datasets():
+    datasets = conn.get_databases()
+    return {"datasets": datasets}
+
+
+@app.post("/add_version/")
+def add_version(version: Version, dataset: Dataset):
+    print(version)
+    dataset_name = dataset.dataset
+    if dataset_name not in datasets:
+        return {'status': '500', 'error': 'dataset inexistente!'}
+
+    dataset_versions = datasets[dataset_name]
+
+    if version.id in dataset_versions:
+        return {'status': '500', 'error': 'versão já existente!'}
+
+    dataset_versions[version.id] = version.owner
+
+    return {"status": "200"}
+
+
+@app.post("/uploadfile/")
+async def upload_file_to_dataset_version(
+    file: UploadFile,
+    file_label: Annotated[str, Form()],
+    dataset: Annotated[str, Form()],
+    version_id: Annotated[str, Form()],
+):
+    if dataset not in datasets:
+        return {'status': '500', 'error': 'dataset inexistente!'}
+
+    dataset_versions = datasets[dataset]
+
+    if version_id in dataset_versions:
+        return {'status': '500', 'error': 'versão já existente!'}
+
+    return {
+        "filename": file.filename,
+        'label': file_label,
+        'dataset': dataset,
+        'version_id': version_id
+    }
