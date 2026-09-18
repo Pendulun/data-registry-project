fef7ae3c994185db5efc79a5a728268a497dd05a
Daniel Campos <danielcampos14@hotmail.com.br>
2026-09-18 16:15:12 -0300

    chore: Update consumed env variables

--- src/app/config/settings.py
+++ src/app/config/settings.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_HOST = os.getenv("PGSQL_HOST")
-POSTGRES_USER = os.getenv("PGSQL_USER")
-POSTGRES_DBNAME = os.getenv("PGSQL_DATABASE")
-POSTGRES_PWD = os.getenv("PGSQL_PASSWORD")
-DSN = f"host={POSTGRES_HOST} dbname={POSTGRES_DBNAME} user={POSTGRES_USER} password={POSTGRES_PWD}"
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_DBNAME = os.getenv("POSTGRES_DB")
+POSTGRES_PWD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+DSN = f"host={POSTGRES_HOST} port={POSTGRES_PORT} dbname={POSTGRES_DBNAME} user={POSTGRES_USER} password={POSTGRES_PWD}"
